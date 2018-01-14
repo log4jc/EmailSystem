cf43470284f92a1cc856592f46a23b3e7fb1b6de
Jason <ozil.liu2@gmail.com>
2018-01-14 23:38:50 +0800

    first commit

---
+++ src/main/java/com/jwebcoder/emailsystem/EmailSystemApplication.java
@@ -0,0 +1,12 @@
+package com.jwebcoder.emailsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jwebcoder/emailsystem/EmailSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jwebcoder.emailsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
